3bb7511ed454a2e34b43c57f2597c08778c7a7df
RioKamerun-Abelo <moussyabel20@gmail.com>
2025-02-19 15:51:04 -0500

    Resolution des conflits

--- main.js
+++ main.js
@@ -20,6 +20,12 @@ var objCarteTuile = null;
 
 var objJoueur = null;
 
+var lingotsRamasses = 0;
+
+var joueurEnMouvement = false;
+
+var objGardes = [];
+
 
 
 /**
@@ -30,14 +36,14 @@ var objJoueur = null;
 
 
 // Changer à cause du nomination
-function initJeu(){
+function initJeu() {
     // Pour débogger
     document.getElementById('monCanvas').style.outline = "thick solid rgb(0, 0, 0)";
 
     console.log('init animation v1');
     objCanvas = document.getElementById('monCanvas')
     objCanvas.focus();
-    
+
 
     objC2D = objCanvas.getContext('2d');
 
@@ -50,9 +56,9 @@ function initJeu(){
 
     dessiner();
     animer();
-}   
+}
 
-function initControlleurJeu(){
+function initControlleurJeu() {
     objControlleurJeu = new Object();
 
     objControlleurJeu.cleHaut = false;
@@ -65,7 +71,7 @@ function initControlleurJeu(){
 
 }
 
-function initMurs(){
+function initMurs() {
     objMurs = new Object();
 
     let largeurMur = 25;
@@ -210,11 +216,11 @@ function initCarteTuile(){
         }
     }
 
-    
+
 }
 
 
-function initJoueur(){
+function initJoueur() {
     objJoueur = new Object();
 
     objJoueur.largeur = 50;
@@ -242,24 +248,24 @@ function initJoueur(){
  *  ----------------------
  */
 
-function animer(){
+function animer() {
     objCycleAnimation = requestAnimationFrame(animer);
 
     // Cycle d'animation
-    effacerDessin();    
+    effacerDessin();
     mettreAjourAnimation();
     dessiner();
 }
 
-function arreterAnimation(){
-    if (objCycleAnimation != null){
+function arreterAnimation() {
+    if (objCycleAnimation != null) {
         cancelAnimationFrame(objCycleAnimation);
     }
     objCycleAnimation = null;
 }
 
-function effacerDessin(){
-    objC2D.clearRect(0,0, objCanvas.width, objCanvas.height); 
+function effacerDessin() {
+    objC2D.clearRect(0, 0, objCanvas.width, objCanvas.height);
 }
 
 /**
@@ -268,7 +274,7 @@ function effacerDessin(){
 *  -----------------
 */
 
-function mettreAjourAnimation(){
+function mettreAjourAnimation() {
 
     miseAJourJoueur();
 }
@@ -321,18 +327,19 @@ function miseAJourJoueur(){
 
     graviteJoueur()
     deplacementJoueur()
+    deplacerGardes();
 }
 
 
-// On doit completmenet le revamp 
-function deplacementJoueur(){
+// Fonctionne plus ou moins (avant le mapping)
+function deplacementJoueur() {
 
     let binDeplacementGauche = false;
     let binDeplacementDroite = false;
     let binDeplacementHaut = false;
     let binDeplacementBas = false;
 
-    if(!objJoueur.binTomber){
+    if (!objJoueur.binTomber) {
 
 
 
@@ -454,25 +461,25 @@ function deplacementJoueur(){
         }
 
     }
-    else{
+    else {
         binDeplacementGauche = false;
         binDeplacementDroite = false;
     }
 
 
-    if(binDeplacementHaut){
+    if (binDeplacementHaut) {
         objJoueur.positionY -= objJoueur.vitesseY
     }
 
-    if(binDeplacementBas){
+    if (binDeplacementBas) {
         objJoueur.positionY += objJoueur.vitesseY
     }
 
-    if(binDeplacementGauche){
+    if (binDeplacementGauche) {
         objJoueur.positionX -= objJoueur.vitesseX;
     }
 
-    if(binDeplacementDroite){
+    if (binDeplacementDroite) {
         objJoueur.positionX += objJoueur.vitesseX;
     }
 
@@ -483,11 +490,11 @@ function deplacementJoueur(){
 }
 
 // A programmer
-function graviteJoueur(){
-    
+function graviteJoueur() {
+
     let binDescend = true;
 
-    if(!(objJoueur.positionY < objMurs.tabMurs[2].yDebut)){
+    if (!(objJoueur.positionY < objMurs.tabMurs[2].yDebut)) {
         binDescend = false;
     }
 
@@ -557,21 +564,46 @@ function graviteJoueur(){
  * -----------------
  */
 
-function dessiner(){
+function dessiner() {
     objC2D.save();
 
 
+    // Dessiner l'environnement 
     dessinerTuiles();
-
     dessinerMurs();
 
-    dessinerJoueur();
+    // Déplacer les gardes avant de les dessiner
+    deplacerGardes();
+
+    // Détecter si le joueur est en mouvement
+    mettreAjourAnimation();
+
+    // Dessin animation des joueurs
+    let frame = Math.floor(performance.now() / 100 % 4);
+
+    let joueurs = [
+        { x: 150, y: 200, couleur: "red" },
+        { x: 250, y: 200, couleur: "green" },
+        { x: 350, y: 200, couleur: "purple" }
+    ];
+
+    joueurs.forEach(joueur => {
+        dessinerJoueur(frame, joueur.couleur, joueur.x, joueur.y);
+    });
+
+    // Dessiner les gardes après mise à jour
+    dessinerGardes();
+
+    // Dessiner les lingots d'or
+    dessinerLingots(100, 150);
+    dessinerLingots(200, 250);
+    dessinerLingots(300, 350);
 
     objC2D.restore();
 }
 
 // Incomplet
-function dessinerTuiles(){
+function dessinerTuiles() {
     objC2D.save();
 
     objC2D.translate(
@@ -596,7 +628,7 @@ function dessinerTuiles(){
     objC2D.restore();
 }
 
-function dessinerUnTuile(strType){
+function dessinerUnTuile(strType) {
     objC2D.fillStyle = `rgb(${Math.random() * 255}, 
     ${Math.random() * 255}, 
     ${Math.random() * 255})`;
@@ -618,7 +650,7 @@ function dessinerUnTuile(strType){
     )
     objC2D.fill();
 
-    if(strType == 'B'){
+    if (strType == 'B') {
         objC2D.fillStyle = 'rgb(92, 92, 92)';
 
         objC2D.beginPath();
@@ -638,12 +670,12 @@ function dessinerUnTuile(strType){
         objC2D.rect(
             padTuile,
             padTuile,
-            objCarteTuile.xLargeurTuile - padTuile*2,
-            objCarteTuile.yLargeurTuile - padTuile*2
+            objCarteTuile.xLargeurTuile - padTuile * 2,
+            objCarteTuile.yLargeurTuile - padTuile * 2
         )
         objC2D.fill();
     }
-    else if(strType == 'P'){
+    else if (strType == 'P') {
 
         // Base du paserelle
         objC2D.fillStyle = 'rgb(72, 72, 72)'
@@ -666,30 +698,30 @@ function dessinerUnTuile(strType){
         objC2D.rect(
             padBrique,
             padBrique,
-            objCarteTuile.xLargeurTuile/2 - padBrique,
-            objCarteTuile.yLargeurTuile/2 - padBrique
+            objCarteTuile.xLargeurTuile / 2 - padBrique,
+            objCarteTuile.yLargeurTuile / 2 - padBrique
         )
         objC2D.fill();
 
         objC2D.beginPath();
         objC2D.rect(
-            objCarteTuile.xLargeurTuile/2 + padBrique,
+            objCarteTuile.xLargeurTuile / 2 + padBrique,
             padBrique,
-            objCarteTuile.xLargeurTuile/2 - padBrique*2,
-            objCarteTuile.yLargeurTuile/2 - padBrique
+            objCarteTuile.xLargeurTuile / 2 - padBrique * 2,
+            objCarteTuile.yLargeurTuile / 2 - padBrique
         )
         objC2D.fill();
 
         objC2D.beginPath();
         objC2D.rect(
             padBrique,
-            objCarteTuile.yLargeurTuile/2 + padBrique,
-            objCarteTuile.xLargeurTuile - padBrique*2,
-            objCarteTuile.yLargeurTuile/2 - padBrique*2
+            objCarteTuile.yLargeurTuile / 2 + padBrique,
+            objCarteTuile.xLargeurTuile - padBrique * 2,
+            objCarteTuile.yLargeurTuile / 2 - padBrique * 2
         )
         objC2D.fill();
     }
-    else if(strType == 'E'){
+    else if (strType == 'E') {
         let largeurBarrePrincipale = 5;
 
         objC2D.fillStyle = 'rgb(191, 147, 51)'
@@ -713,13 +745,13 @@ function dessinerUnTuile(strType){
             objCarteTuile.xLargeurTuile,
             objCarteTuile.yLargeurTuile
         )
-        objC2D.fill();        
+        objC2D.fill();
 
         // Ajouter les petits barres
         for(let i = 5; i < objCarteTuile.yLargeurTuile; i += 10){
             objC2D.beginPath();
-            objC2D.moveTo(0,i);
-            objC2D.lineTo(objCarteTuile.xLargeurTuile,i);
+            objC2D.moveTo(0, i);
+            objC2D.lineTo(objCarteTuile.xLargeurTuile, i);
             objC2D.stroke();
         }
 
@@ -742,7 +774,7 @@ function dessinerUnTuile(strType){
 }
 
 
-function dessinerMurs(){
+function dessinerMurs() {
     objC2D.save();
 
     objC2D.fillStyle = objMurs.strCouleur;
@@ -761,9 +793,203 @@ function dessinerMurs(){
     objC2D.restore();
 }
 
+function dessinerJoueur(frame, couleurCorps) {
+    objC2D.save();
+
+    let x = objJoueur.positionX;
+    let y = objJoueur.positionY;
+
+    // Définitiion de la couleur de la tenue du personnage
+    objC2D.fillStyle = couleurCorps || "red";
+
+    // Corps (inclut bras et tronc)
+    objC2D.fillRect(x + 4, y + 8, 12, 16);
+
+    // Tête (avec casque jaune)
+    objC2D.fillStyle = "#FFA500"; // Jaune orangé
+    objC2D.fillRect(x + 6, y, 8, 8);
+
+    // Casque cyan
+    objC2D.fillStyle = "cyan";
+    objC2D.fillRect(x + 8, y - 2, 4, 4);
+
+    if (joueurEnMouvement) {
+        let mouvement = frame % 4;
+
+        // Animation des jambes
+        let jambeGaucheX = mouvement < 2 ? x + 4 : x + 6;
+        let jambeDroiteX = mouvement < 2 ? x + 10 : x + 8;
+        let jambeGaucheY = mouvement < 2 ? y + 26 : y + 24;
+        let jambeDroiteY = mouvement < 2 ? y + 24 : y + 26;
+
+        objC2D.fillStyle = "white";
+        objC2D.fillRect(jambeGaucheX, jambeGaucheY, 5, 10);
+        objC2D.fillRect(jambeDroiteX, jambeDroiteY, 5, 10);
+
+        // Animation des bras
+        let brasGaucheX = mouvement < 2 ? x + 2 : x + 5;
+        let brasDroiteX = mouvement < 2 ? x + 13 : x + 10;
+        let brasGaucheY = mouvement < 2 ? y + 12 : y + 6;
+        let brasDroiteY = mouvement < 2 ? y + 6 : y + 12;
+
+        objC2D.fillStyle = couleurCorps;
+        objC2D.fillRect(brasGaucheX, brasGaucheY, 5, 6);
+        objC2D.fillRect(brasDroiteX, brasDroiteY, 5, 6);
+    } else {
+        // Position immobile
+        objC2D.fillStyle = "white";
+        objC2D.fillRect(x + 5, y + 24, 5, 10); // Jambe gauche
+        objC2D.fillRect(x + 10, y + 24, 5, 10); // Jambe droite
+
+        objC2D.fillStyle = couleurCorps;
+        objC2D.fillRect(x + 2, y + 10, 5, 6); // Bras gauche
+        objC2D.fillRect(x + 13, y + 10, 5, 6); // Bras droit
+    }
+
+    objC2D.restore();
+}
 
 
-function dessinerJoueur(){
+
+var objGardes = [];
+
+function initGardes(nombreGardes) {
+    objGardes = [];
+    let couleursGardes = ["red", "green", "purple"]; // Couleurs des chandails des gardes
+    let positionsUtilisees = new Set();
+
+    let passerellesDisponibles = objCarteTuile.tabTuile.filter(tuile => tuile.type === 'P');
+
+    for (let i = 0; i < nombreGardes; i++) {
+        let positionValide = false;
+        let positionGarde = null;
+
+        while (!positionValide) {
+            let tuile = passerellesDisponibles[Math.floor(Math.random() * passerellesDisponibles.length)];
+            let positionX = tuile.tuileX * objCarteTuile.xLargeurTuile;
+            let positionY = tuile.tuileY * objCarteTuile.yLargeurTuile - 50;
+
+            let clePosition = `${positionX}-${positionY}`;
+
+            if (!positionsUtilisees.has(clePosition) && !lingotsRamasses.includes(clePosition)) {
+                positionGarde = {
+                    x: positionX,
+                    y: positionY,
+                    couleur: couleursGardes[i % couleursGardes.length],
+                    vitesseX: 1,
+                    vitesseY: 2,
+                    direction: Math.random() < 0.5 ? "gauche" : "droite",
+                    enTrou: false,
+                    tempsDansTrou: 0
+                };
+                positionsUtilisees.add(clePosition);
+                positionValide = true;
+            }
+        }
+        objGardes.push(positionGarde);
+    }
+}
+
+function dessinerGardes() {
+    objGardes.forEach(garde => {
+        dessinerPersonnage(garde.x, garde.y, garde.couleur);
+    });
+}
+
+// Fonction qui gère le déplacement et la chute des gardes
+function deplacerGardes() {
+    objGardes.forEach(garde => {
+        if (garde.enTrou) {
+            garde.tempsDansTrou++;
+
+            if (garde.tempsDansTrou >= 240) { // 4 secondes (60 FPS * 4)
+                if (trouRebouche(garde.x, garde.y)) {
+                    reinitialiserGarde(garde);
+                } else {
+                    garde.enTrou = false;
+                }
+            }
+            return;
+        }
+
+        // Vérifier si le garde est en train de tomber
+        if (gardeTombe(garde)) {
+            garde.y += garde.vitesseY;
+            return;
+        }
+
+        // Déplacement horizontalement en évitant les obstacles
+        let nouvellePositionX = garde.x + (garde.direction === "gauche" ? -garde.vitesseX : garde.vitesseX);
+
+        if (!collisionMur(nouvellePositionX, garde.y) && !trouDetecte(nouvellePositionX, garde.y)) {
+            garde.x = nouvellePositionX;
+        } else {
+            garde.direction = garde.direction === "gauche" ? "droite" : "gauche";
+        }
+    });
+}
+
+// Vérification si un garde doit tomber
+function gardeTombe(garde) {
+    let positionDessous = { x: garde.x, y: garde.y + 50 };
+
+    return !collisionPasserelle(positionDessous) && !collisionBarre(positionDessous);
+}
+
+// Vérification si un garde touche une passerelle
+function collisionPasserelle(position) {
+    return objCarteTuile.tabTuile.some(tuile =>
+        tuile.type === 'P' && position.y === tuile.tuileY * objCarteTuile.yLargeurTuile
+    );
+}
+
+// Vérification si un garde touche une barre de franchissement
+function collisionBarre(position) {
+    return objCarteTuile.tabTuile.some(tuile =>
+        tuile.type === 'E' && position.y === tuile.tuileY * objCarteTuile.yLargeurTuile
+    );
+}
+
+// Vérification si un garde se trouve devant un trou
+function trouDetecte(x, y) {
+    return objCarteTuile.tabTuile.some(tuile =>
+        tuile.type === 'V' && tuile.tuileX * objCarteTuile.xLargeurTuile === x &&
+        tuile.tuileY * objCarteTuile.yLargeurTuile === y + 50
+    );
+}
+
+// Vérification si un garde touche un mur
+function collisionMur(x, y) {
+    return objMurs.tabMurs.some(mur => x >= mur.xDebut && x <= mur.xFin && y >= mur.yDebut && y <= mur.yFin);
+}
+
+// Vérification si un trou a été rebouché
+function trouRebouche(x, y) {
+    return objCarteTuile.tabTuile.some(tuile =>
+        tuile.type === 'B' && tuile.tuileX * objCarteTuile.xLargeurTuile === x &&
+        tuile.tuileY * objCarteTuile.yLargeurTuile === y
+    );
+}
+
+// Réinitialisation de la position du garde s'il est mort dans un trou rebouché
+function reinitialiserGarde(garde) {
+    let positionsDisponibles = objCarteTuile.tabTuile.filter(tuile => tuile.tuileY === 2);
+    let tuile = positionsDisponibles[Math.floor(Math.random() * positionsDisponibles.length)];
+    garde.x = tuile.tuileX * objCarteTuile.xLargeurTuile;
+    garde.y = tuile.tuileY * objCarteTuile.yLargeurTuile - 50;
+    garde.enTrou = false;
+    garde.tempsDansTrou = 0;
+}
+
+// Fonction principale d'animation
+function animer() {
+    objCycleAnimation = requestAnimationFrame(animer);
+    effacerDessin();
+    deplacerGardes();
+    dessiner();
+}
+
+/*function dessinerJoueur(frame) {
     objC2D.save();
 
     objC2D.translate(
@@ -784,18 +1010,48 @@ function dessinerJoueur(){
         objJoueur.largeur,
         objJoueur.hauteur
     )
+    
     objC2D.fill();
 
+    objC2D.restore();
+}*/
 
-    // Debug
+
+
+function dessinerLingots(x, y) {
+    objC2D.save();
+
+    // Couleur dorée
+    objC2D.fillStyle = "#FFD700";
+
+    // Amas de lingots avec plusieurs petites briques
+    let largeur = 10; // Largeur d'un petit lingot
+    let hauteur = 8;  // Hauteur d'un petit lingot
+    let lignes = 3;    // Nombre de lignes de lingots
+    let colonnes = 4;  // Nombre de colonnes de lingots
+
+    for (let i = 0; i < lignes; i++) {
+        for (let j = 0; j < colonnes - i; j++) { // Réduction du nombre de lingots sur chaque ligne pour un effet pyramidal
+            let offsetX = x + j * largeur + (i * largeur / 2);
+            let offsetY = y - i * hauteur;
+
+            // Base du lingot
+            objC2D.fillStyle = "#FFD700";
+            objC2D.fillRect(offsetX, offsetY, largeur, hauteur);
+
+            // Ombre pour effet 3D
+            objC2D.fillStyle = "#DAA520";
+            objC2D.fillRect(offsetX + largeur / 3, offsetY + hauteur / 3, largeur / 1.5, hauteur / 1.5);
+        }
+    }
+
+    // Effet de brillance sur le haut des lingots
+    objC2D.fillStyle = "#FFFACD";
     objC2D.beginPath();
-    objC2D.arc(0, 0, 5, 0, 2 * Math.PI);
-    objC2D.fillStyle = "red";
+    objC2D.arc(x + 20, y - 5, 4, 0, Math.PI * 2);
     objC2D.fill();
-    objC2D.lineWidth = 4;
-    objC2D.strokeStyle = "blue";
-    objC2D.stroke();
 
+
+    objC2D.restore();
 }
-
 
